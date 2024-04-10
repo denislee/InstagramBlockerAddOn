40455d43318dc5d456239a11005722cc9cda2ae1
Denis Lee <denislee@gmail.com>
2024-04-10 08:02:04 -0300

    remove sponsored posts

--- blocker.js
+++ blocker.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function() {
   viewOlderPostElement = null;
 
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].textContent === 'View older posts') {
+    if (elements[i].textContent === 'Suggested for you' || elements[i].textContent === 'View older posts' || elements[i].textContent === 'patrocinado' || elements[i].textContent === 'Follow') {
       viewOlderPostElement = elements[i];
       break;
     }
@@ -13,9 +13,9 @@ window.addEventListener('scroll', function() {
   rootElement = viewOlderPostElement?.parentElement?.parentElement?.parentElement;
   rootElement.style.border = '5px solid red'
 
-
   document.body.style.maxHeight = rootElement.offsetTop + 'px';
   if (rootElement.getBoundingClientRect().top <= windowHeight - rootElement.offsetHeight) {
     window.scrollTo(0, rootElement.offsetTop - windowHeight + rootElement.offsetHeight);
   }
+
 });
