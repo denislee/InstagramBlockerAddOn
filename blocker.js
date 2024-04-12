7b6179501800173cfce1bfff769e3551cc4fc018
Denis Lee <denislee@gmail.com>
2024-04-12 07:58:37 -0300

    fix

--- blocker.js
+++ blocker.js
@@ -4,15 +4,28 @@ window.addEventListener('scroll', function() {
   let viewOlderPostElement = null;
 
   for (let i = 0; i < elements.length; i++) {
-    if (['Suggested for you', 'View older posts', 'patrocinado', 'Follow'].includes(elements[i].textContent)) {
+    if (elements[i].textContent === 'View older posts') {
       viewOlderPostElement = elements[i];
-      break;
+      rootElement = viewOlderPostElement?.parentElement?.parentElement?.parentElement;
+      rootElement.style.border = '5px solid red'
+      if (rootElement && rootElement.getBoundingClientRect().top <= windowHeight - rootElement.offsetHeight) {
+        window.scrollTo(0, rootElement.offsetTop - windowHeight + rootElement.offsetHeight);
+      }
+    }
+
+    if (elements[i].textContent === 'Follow' ||
+      elements[i].textContent.includes('Follow') ||
+      elements[i].textContent.includes('Suggested for you') ||
+      elements[i].textContent.includes('Suggested Posts') ||
+      elements[i].textContent.includes('Because you')) {
+      viewOlderPostElement = elements[i];
+      elements[i].style.border = '5px solid blue'
+      rootElement = viewOlderPostElement?.parentElement?.parentElement?.parentElement;
+      rootElement.style.border = '5px solid red'
+      //rootElement.style.visibility = 'hidden'; // Makes content invisible but keeps space
+      rootElement.style.display = 'none'; // This line hides the rootElement
     }
   }
 
-  const rootElement = viewOlderPostElement?.parentElement?.parentElement?.parentElement;
-  if (rootElement && rootElement.getBoundingClientRect().top <= windowHeight - rootElement.offsetHeight) {
-    rootElement.remove(); // This line replaces setting the border and modifying the maxHeight
-  }
-});
+ });
 
